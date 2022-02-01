cf24376838d6ec2686897d48ae621109df0aa3a2
Kizuwe1lf <serdar@goodjobgames.com>
2022-02-02 01:00:41 +0300

    feat: Implemented tunnel gRPC endpoints

--- api/api.go
+++ api/api.go
@@ -2,13 +2,33 @@ package api
 
 import (
 	"context"
+	"tunnel/client"
 	pb "tunnel/proto/gen/v1"
 )
 
-type ServiceImplementation struct {
+type Service struct {
 	pb.UnimplementedTunnelServiceServer
+	twitter client.Twitter
 }
 
-func (s *ServiceImplementation) PostTweet(ctx context.Context, request pb.PostTweetRequest) (pb.PostTweetResponse, error) {
-	return pb.PostTweetResponse{}, nil
+func (s *Service) PostTweet(ctx context.Context, request *pb.PostTweetRequest) (*pb.PostTweetResponse, error) {
+	mediaID, err := s.twitter.UploadMedia(request.Image)
+
+	if err != nil {
+		return &pb.PostTweetResponse{}, err
+	}
+
+	err = s.twitter.PostMediaTweet(mediaID)
+
+	if err != nil {
+		return &pb.PostTweetResponse{}, err
+	}
+
+	return &pb.PostTweetResponse{}, nil
+}
+
+func NewService(token, tokenSecret, consumerKey, consumerSecret string) *Service {
+	return &Service{
+		twitter: client.NewClient(token, tokenSecret, consumerKey, consumerSecret),
+	}
 }
